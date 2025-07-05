ae0efc2dc3b9aa533b96f5acb0c1dda967007579
Akshat Dwivedi <111682904+akshatdwivedi24@users.noreply.github.com>
2025-07-05 11:31:25 +0530

    Add files via upload

---
+++ Teckniv/src/main/java/com/teckniv/teckniv/TecknivApplication.java
@@ -0,0 +1,13 @@
+package com.teckniv.teckniv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecknivApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TecknivApplication.class, args);
+    }
+
+}
---
+++ Teckniv/src/test/java/com/teckniv/teckniv/TecknivApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teckniv.teckniv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecknivApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
